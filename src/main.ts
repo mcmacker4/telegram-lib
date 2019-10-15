b50b0ace4cbbe74902f32d9e29ee533c0a5b549d
Hans Geel <mcmacker4@gmail.com>
2019-10-15 10:55:30 +0200

    Removed test code

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,1 @@
-import { Client, ClientConfig } from "./client/client";
-
-// Example test client
-
-const config = require('../bot.json') as ClientConfig
-const client = new Client(config)
-
-client.api.getMe().then(me => {
-    console.log(`My name is ${me.first_name}`)
-}).catch(err => {
-    console.error(err)
-})
-
-
 export * from './client/client'
